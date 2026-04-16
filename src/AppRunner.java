6ca3e856880b4f00e799ec3abc116112499bf855
Arlen <sherimbaevarlen@gmail.com>
2026-04-16 16:32:16 +0600

    changed the method chooseAction() in the class AppRunner.java

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -62,7 +62,8 @@ public class AppRunner {
         print(" h - Выйти");
         String action = fromConsole().substring(0, 1);
         if ("a".equalsIgnoreCase(action)) {
-
+            acceptBill();
+            return;
         }
         try {
             for (int i = 0; i < products.size(); i++) {
